8ad00c333c1f6fd4d3aab27fd73f6c5236d55962
YaredFall <61197339+YaredFall@users.noreply.github.com>
2025-07-02 21:02:53 +0700

    chore: add prettier config

---
+++ prettier.config.mjs
@@ -0,0 +1,6 @@
+/** @type {import('prettier').Config & import('prettier-plugin-tailwindcss').PluginOptions} */
+export default {
+    printWidth: 120,
+    tabWidth: 4,
+    plugins: ["prettier-plugin-tailwindcss"],
+};
